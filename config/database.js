9e9a07d4ab912a61fdc3ccb421b5310ea626ec1b
Abner André <abner@oialbert.com.br>
2022-07-20 20:09:51 -0300

    update

--- config/database.js
+++ config/database.js
@@ -2,10 +2,18 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: 'ec2-54-87-179-4.compute-1.amazonaws.com',
+      port: '5432',
+      database: 'd4caucfkqm8rvl',
+      user: 'zttukljgvszbsg',
+      password: '37d3f04f5f597ff6075f183193f52bba0f17a4e2458081e1bcbc589cf1765601',
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
     useNullAsDefault: true,
   },
 });
